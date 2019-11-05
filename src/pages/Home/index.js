ae085f865b40f3def8dd8c50357d1ff2bf419c97
LORDBABUINO <jorge.x7@gmail.com>
2019-11-05 08:21:17 -0300

    Styles Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,101 @@
 import React from 'react'
+import { MdAddShoppingCart } from 'react-icons/md'
 
-import { Container } from './styles'
+import { ProductList } from './styles'
 
 export default function Home() {
-  return <Container >
-    <h1>Home</h1>
-    </Container>
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="white" /> 3
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="white" /> 3
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="white" /> 3
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="white" /> 3
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="white" /> 3
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="white" /> 3
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  )
 }
