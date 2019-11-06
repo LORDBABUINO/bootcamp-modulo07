f5949c30a58fe87311253b6bf50fc5138dc6072e
LORDBABUINO <jorge.x7@gmail.com>
2019-11-06 20:05:13 -0300

    Gets data from API and formats price

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,101 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
+import api from '../../services/api'
+import { formatPrice } from '../../util/format'
 
 import { ProductList } from './styles'
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
+export default class Home extends Component {
+  state = {
+    products: [],
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="white" /> 3
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
+  async componentDidMount() {
+    const response = await api.get('products')
+    const data = response.data.map(product => ({
+      ...product,
+      price: formatPrice(product.price),
+    }))
+    this.setState({ products: data })
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="white" /> 3
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
+  render() {
+    const { products } = this.state
+    return (
+      <ProductList>
+        {products.map(({ id, image, title, price }) => (
+          <li key={id}>
+            <img src={image} alt={title} />
+            <strong>{title}</strong>
+            <span>{price}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="white" /> 3
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="white" /> 3
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="white" /> 3
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/12/004-5830-012/004-5830-012_detalhe1.jpg?ims=240x240"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$ 129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="white" /> 3
-          </div>
-          <span>adicionar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  )
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="white" /> 3
+              </div>
+              <span>adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    )
+  }
 }
